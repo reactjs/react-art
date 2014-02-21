95cc37dbcc7a1b56f81b8a49616c8f9718e7d4a3
Ben Alpert <spicyjalapeno@gmail.com>
2014-02-21 13:54:54 -0800

    Add VectorWidget example using webpack

---
+++ examples/vector-widget/README.md
@@ -0,0 +1,10 @@
+# VectorWidget example
+
+To try this example, run:
+
+```
+npm install
+webpack app.js bundle.js
+```
+
+in this directory, then open index.html in your browser.
---
+++ examples/vector-widget/app.js
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require('react');
+var VectorWidget = require('./VectorWidget');
+
+React.renderComponent(<VectorWidget />, document.body);
