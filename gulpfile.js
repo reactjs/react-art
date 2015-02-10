3e41745711b3dd7dcb31ca984de51a27e4a42869
Kyle Mathews <mathews.kyle@gmail.com>
2015-02-09 19:24:32 -0800

    Add gulp-styled shell script to build and publish examples website
    Fixes #39

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gReact = require('gulp-react');
 var del = require('del');
+var shell = require('gulp-shell');
 
 gulp.task('clean', function(cb) {
   del(['lib/', 'Flux.js'], cb);
@@ -12,3 +13,14 @@ gulp.task('default', ['clean'], function() {
              .pipe(gulp.dest('lib'));
 
 });
+
+gulp.task('build-examples-website', ['clean'], shell.task([
+  'git checkout gh-pages',
+  'git merge master',
+  'cd examples/vector-widget; ../../node_modules/.bin/webpack app.js bundle.js',
+  'cp -r examples/* .',
+  'git add --all .',
+  'git commit -m "Update website"',
+  'git push origin gh-pages',
+  'git checkout master'
+]));
