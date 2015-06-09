f0015af6ec09fcadea4a0ac4daaf8a5635725952
Paul O’Shannessy <paul@oshannessy.com>
2015-06-09 11:51:24 -0700

    Update to work with directory outside of cwd

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,21 @@ gulp.task('default', ['clean'], function() {
 
 });
 
-gulp.task('build-examples-website', ['clean'], shell.task([
-  'git checkout gh-pages',
-  'git merge master',
-  'cd examples/vector-widget; ../../node_modules/.bin/webpack app.js bundle.js',
-  'cp -r examples/* .',
-  'git add --all .',
+gulp.task('build-examples-website', ['clean'], shell.task([[
+  'pushd ../react-art-gh-pages',
+  'git checkout -- .',
+  'git clean -dfx',
+  'git pull',
+  'rm -rf *',
+  'popd',
+  'pushd examples/vector-widget',
+  'npm install',
+  'npm run build',
+  'popd',
+  'cp -r examples/vector-widget/{index.html,bundle.js} ../react-art-gh-pages',
+  'pushd ../react-art-gh-pages',
+  'git add -A .',
   'git commit -m "Update website"',
-  'git push origin gh-pages',
-  'git checkout master'
-]));
+  'popd',
+  'echo "react-art-gh-pages ready to push"'
+].join(';')]));
