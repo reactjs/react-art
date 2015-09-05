85ff6cdd6800027432fe0de94b7b39a84b939a4f
Ben Alpert <balpert@fb.com>
2015-09-04 17:18:23 -0700

    Use NODE_ENV=development in example

--- examples/vector-widget/webpack.config.js
+++ examples/vector-widget/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: JSON.stringify('production')}
+      'process.env': {NODE_ENV: JSON.stringify('development')}
     })
   ]
 };
